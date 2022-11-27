cf9cbdf22c0c5dc633c801ce096d1eb7d0cb417f
Jhoopa <jhoopadev@gmail.com>
2022-11-27 18:41:28 -0300

    adicionado index.html, e colocado como resposta do app.get('/')

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const app = express()
 const port = 25565;
 
 app.get('/', (req, res) => {
-    res.send(`
-    <body style="background: black;">
-        <h1 style="color: white; font-family: monospaced;">Hello World!</h1>
-    </body>
-    `)
+    res.sendFile(__dirname+'/index.html')
 })
 
 app.listen(port, () => {
