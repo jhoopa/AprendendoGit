62f0db3a456fe3371d4f17f658afc12371e8a13f
jhoopa <jhoopadev@gmail.com>
2022-11-28 12:58:37 -0300

    index.html remodelado

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 8080;
 app.use(express.static('./public/'))
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html')
+    res.sendFile('./index.html')
 })
 
 app.listen(port, () => {
